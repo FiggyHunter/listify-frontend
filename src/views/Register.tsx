35bb1c0f962c8dbab59e36c3ed5d00273bbf37ab
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-31 04:13:28 +0100

    ci-cd test

--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+import "../App.css";
+
 const Login = () => {
-  return <main className="">test</main>;
+  return (
+    <main className="">
+      Wow, azure! <Link to="/login">Login</Link>
+    </main>
+  );
 };
 
 export default Login;
