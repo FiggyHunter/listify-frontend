41695e38e710efbbbab9c101fde03f46a889c7b8
FiggyHunter <leonardo.roi3@gmail.com>
2024-01-04 02:15:59 +0100

    Started working on a login view/page
    The standard for naming the routes will be views. In this view, I implemented a basic layout of semantic HTML tags.

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,16 +1,39 @@
 import { Link } from "react-router-dom";
+import LoginForm from "../components/login/LoginForm.tsx";
 
 const Login = () => {
   return (
-    <main className="">
-      {" "}
-      <div>
-        {/* Navigation using Link component */}
-        <Link className="" to="/">
-          Home
-        </Link>
-        <Link to="/register">Register</Link>
-      </div>
+    <main className="h-screen w-screen grid items-center bg-bkg">
+      <img
+        className="place-self-center sm:w-36 md:w-72"
+        src="/logo.svg"
+        alt="logo for the app"
+      />
+      <LoginForm />
+      <section className="flex flex-row items-center gap-2 justify-center ">
+        <p className="text-content w-max ">Powered by </p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="21"
+          viewBox="0 0 28 21"
+          fill="none"
+        >
+          <rect
+            x="16.764"
+            y="15.6779"
+            width="11.2359"
+            height="4.49436"
+            className="fill-content"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M12.9085 10.2495L3.24874 0.589722L0.0707485 3.76772L6.62758 10.3245L0.0707397 16.8814L3.24873 20.0594L12.9085 10.3996L12.8335 10.3246L12.9085 10.2495Z"
+            className="fill-content"
+          />
+        </svg>
+      </section>
     </main>
   );
 };
