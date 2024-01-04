0b31327980c9f24c6f73cf6f686004800a8ed80e
FiggyHunter <leonardo.roi3@gmail.com>
2024-01-04 02:03:39 +0100

    Added a new tailwind config file
    In this file, I wrote, and will write all the configuration needed for tailwind, including custom colors, importing fonts and implementing other relevant styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,43 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "200px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+    },
+    extend: {
+      width: {
+        "70p": "70%",
+        "75p": "75%",
+      },
+      colors: {
+        accent: {
+          1: "var(--color-accent1)",
+          2: "var(--color-accent2)",
+        },
+        bkg: "var(--color-bkg)",
+        content: "var(--color-content)",
+        transparentFill: "var(--input-fill)",
+        crimson: "#E56B6F",
+        crimsonHover: "#D85C60",
+        darkBlue: "#2E5077",
+        darkBlueHover: "#1D3550",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      height: {
+        screen: ["100vh /* fallback for Opera, IE and etc. */", "100dvh"],
+        fill: ["100vh /* fallback for Opera, IE and etc. */", "100dvh"],
+      },
+    },
   },
+
   plugins: [],
 };
