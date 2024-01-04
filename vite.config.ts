76cf25a47b1f7085b5ce400982ce44aacd981ed2
FiggyHunter <leonardo.roi3@gmail.com>
2024-01-04 02:05:02 +0100

    Added the vite config file
    In this config file, I'm hosting the application on an https server, so I can simulate the real world experience, first hand. I'm using mkcert to create a cert when running npm run preview and npm run dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    https: true,
+  },
+  plugins: [react(), mkcert()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
