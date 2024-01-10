8cfe657dd5e55353cf779a9c4009c51436ccd1d4
FiggyHunter <leonardo.roi3@gmail.com>
2024-01-11 00:37:25 +0100

    added new tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from "tailwindcss/plugin";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -13,6 +15,14 @@ export default {
       // => @media (min-width: 1024px) { ... }
     },
     extend: {
+      fontSize: {
+        xsm: "0.8rem",
+      },
+      borderWidth: {
+        1: "0.25px",
+        2: "0.75px",
+        3: "2px",
+      },
       width: {
         "70p": "70%",
         "75p": "75%",
@@ -23,12 +33,15 @@ export default {
           2: "var(--color-accent2)",
         },
         bkg: "var(--color-bkg)",
+        bkgContrast: "var(--bkg-contrast)",
         content: "var(--color-content)",
         transparentFill: "var(--input-fill)",
         crimson: "#E56B6F",
         crimsonHover: "#D85C60",
         darkBlue: "#2E5077",
         darkBlueHover: "#1D3550",
+        labelFill: "var(--text-label-fill)",
+        labelBkg: "var(--label-bkg)",
       },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
@@ -39,6 +52,31 @@ export default {
       },
     },
   },
-
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, addComponents, addUtilities, theme }) {
+      addUtilities({
+        ".h-my-screen": {
+          minHeight: ["100vh /* fallback for Opera, IE and etc. */", "100dvh"],
+        },
+        ".min-h-half": {
+          minHeight: ["50%"],
+        },
+        ".max-h-half": {
+          maxHeight: ["50%"],
+        },
+        ".custom-cols-dash": {
+          gridTemplateColumns: "2fr 6fr",
+        },
+        ".custom-cols-company": {
+          gridTemplateColumns: "2fr 7fr",
+        },
+        ".custom-rows": {
+          gridAutoRows: "max-content",
+        },
+        ".custom-rows-2": {
+          gridTemplateRows: "min-content 1fr",
+        },
+      });
+    }),
+  ],
 };
