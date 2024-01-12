04ccf1c1c0a1ac51834b93ac02189cea6f868e86
FiggyHunter <leonardo.roi3@gmail.com>
2024-01-12 19:27:24 +0100

    team deployment

--- src/views/Account.tsx
+++ src/views/Account.tsx
@@ -1,13 +1,15 @@
 import Navigation from "@/components/shared/Navigation";
+import { useJwtStore } from "@/stores/useUserStore";
 import React from "react";
 
 const Account = () => {
+  const { jwt, setJwt } = useJwtStore();
   return (
     <>
       <Navigation />
       <div
         onClick={() => {
-          // sign out logic
+          setJwt("");
         }}
         className="grid h-screen w-screen place-content-center bg-red-400"
       >
