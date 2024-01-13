9b4037f5c06d3854eb133fd672355d4dc6f5e232
FiggyHunter <leonardo.roi3@gmail.com>
2024-01-13 10:03:59 +0100

    added sign out feature

--- src/views/Account.tsx
+++ src/views/Account.tsx
@@ -1,9 +1,17 @@
 import Navigation from "@/components/shared/Navigation";
 import { useJwtStore } from "@/stores/useUserStore";
 import React from "react";
+import { useJwt } from "react-jwt";
+import { useNavigate } from "react-router-dom";
 
 const Account = () => {
+  const navigate = useNavigate();
   const { jwt, setJwt } = useJwtStore();
+  const token = useJwt(jwt) || null;
+  if (!jwt || jwt === "" || jwt === "noToken") {
+    navigate("/login");
+    return;
+  }
   return (
     <>
       <Navigation />
@@ -11,7 +19,7 @@ const Account = () => {
         onClick={() => {
           setJwt("");
         }}
-        className="grid h-screen w-screen place-content-center bg-red-400"
+        className="-mt-4 grid h-screen w-screen place-content-center bg-red-400"
       >
         123
       </div>
