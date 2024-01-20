f98f2a953d0269698725b31ca7d9d0462daf0577
FiggyHunter <leonardo.roi3@gmail.com>
2024-01-20 03:30:34 +0100

    visual fixes

--- src/components/shared/DarkModeToggle.tsx
+++ src/components/shared/DarkModeToggle.tsx
@@ -16,7 +16,7 @@ const DarkModeToggle = () => {
     >
       {darkMode ? (
         <svg
-          className="fill-black"
+          className="fill-content"
           xmlns="http://www.w3.org/2000/svg"
           width="24"
           height="24"
@@ -27,6 +27,7 @@ const DarkModeToggle = () => {
         </svg>
       ) : (
         <svg
+          className="fill-content"
           xmlns="http://www.w3.org/2000/svg"
           width="24"
           height="24"
