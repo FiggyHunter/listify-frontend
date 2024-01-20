a7cd7c127d81ea7aa4aa1141c261555e641af320
FiggyHunter <leonardo.roi3@gmail.com>
2024-01-20 03:31:15 +0100

    custom handling of errors

--- src/utilities/ResponseErrors.ts
+++ src/utilities/ResponseErrors.ts
@@ -1,1 +1,18 @@
 // We will get an error from the backend, and display it in a secure and context-appropriate manner to the frontend
+export const handleResponseError = (error, setter) => {
+  if (error === "Register") {
+    setter({
+      email: "There has been an error processing your request.",
+      firstName: "There has been an error processing your request.",
+      lastName: "There has been an error processing your request.",
+      password: "There has been an error processing your request.",
+      repeatPassword: "There has been an error processing your request.",
+    });
+    return;
+  }
+
+  setter({
+    email: "Invalid creditentials",
+    password: "Invalid creditentials",
+  });
+};
