9b4987c143f1ef4b2c8fc041b9ba45b5380bc9be
FiggyHunter <leonardo.roi3@gmail.com>
2024-01-24 16:42:34 +0100

    general layout visual bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
         <meta name="twitter:image" content="https://example.com/image.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
-      <div className="h-screen bg-bkg w-full overflow-y-scroll ">
+      <div className="h-my-screen bg-bkg w-full ">
         <BrowserRouter>
           <AppRoutes />
         </BrowserRouter>
