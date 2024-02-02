6c450b94faeb309401c6491df4a8036f452a872b
FiggyHunter <leonardo.roi3@gmail.com>
2024-02-02 19:58:24 +0100

    Added access request component

---
+++ src/components/admin/AccessRequest.tsx
@@ -0,0 +1,21 @@
+const AccessRequest = ({ user }) => {
+  console.log(user);
+  return (
+    <section className="grid w-full px-8 mx-auto access-grid gap-4 ">
+      <div className="h-16  sm:col-span-2 lg:col-span-1  sm:self-center w-16 bg-sky-400 "></div>
+      <div className="flex sm:col-span-2 lg:col-span-1 flex-col gap-3 w-full text-black">
+        <h2 className="text-xl font-bold">{`${user.name} ${user.surname}`}</h2>
+        <h3 className="text-base font-light">Email: {user.email}</h3>
+        <h4 className="text-base font-light">Date: {user.createdAt}</h4>
+      </div>
+      <div className="flex items-center gap-4">
+        <button className="bg-darkBlue">Disband</button>
+      </div>{" "}
+      <div className="flex items-center gap-4">
+        <button className="bg-crimson">Admit</button>
+      </div>{" "}
+    </section>
+  );
+};
+
+export default AccessRequest;
