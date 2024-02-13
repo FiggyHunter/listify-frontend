891f059a1bfe57e42d334d548e2b94e57416a80f
FiggyHunter <leonardo.roi3@gmail.com>
2024-02-13 22:07:37 +0100

    small visual adjustments

--- src/components/company/Employees.tsx
+++ src/components/company/Employees.tsx
@@ -1,6 +1,6 @@
 const Employees = () => {
   return (
-    <div className="text-content flex w-5/6 mx-auto gap-2 items-center my-4">
+    <div className="text-content flex w-5/6 mx-auto gap-2 sm:justify-center md:justify-start my-4">
       <div className="bg-gray-300 self-center w-16 h-16 rounded-full "></div>
       <div className="">
         <p className="font-extrabold">Name Nameson</p> <p>3 Months</p>
