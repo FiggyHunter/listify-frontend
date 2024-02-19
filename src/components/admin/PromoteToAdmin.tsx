55897c955f6d8a003dd3ae398e72c410cb94359f
FiggyHunter <leonardo.roi3@gmail.com>
2024-02-19 18:26:59 +0100

    added promote to admin button

---
+++ src/components/admin/PromoteToAdmin.tsx
@@ -0,0 +1,12 @@
+const PromoteToAdmin = () => {
+  return (
+    <button
+      onClick={() => {}}
+      className="focus:outline-none  rounded-lg hover:bg-crimsonHover hover:outline-0 border-none transition-all duration-250 bg-crimson font-semibold text-white"
+    >
+      Promote to Admin
+    </button>
+  );
+};
+
+export default PromoteToAdmin;
