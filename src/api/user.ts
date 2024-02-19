d624f258de373c317b861be7f139eee3000cffbe
FiggyHunter <leonardo.roi3@gmail.com>
2024-02-19 18:25:47 +0100

    multiple user endpoints implemented

--- src/api/user.ts
+++ src/api/user.ts
@@ -6,6 +6,9 @@ const notify = () => toast(`You have saved the updates to your user account.`);
 const notifyApprovedUser = (userFullName) =>
   toast(`You have successfully approved the request for ${userFullName}.`);
 
+const notifyDisbandedRequest = () =>
+  toast(`You have successfully DISBANDED the request to join.`);
+
 export const getUserById = async (userId, jwt) => {
   const uri =
     import.meta.env.VITE_API_ENDPOINT + `/api/users/getById/${userId}`;
@@ -142,6 +145,134 @@ export const changeAdmitted = async (
     return response.data;
   } catch (error) {
     setButtonLoading(buttonId, false);
+    console.log(error);
+    throw error;
+  }
+};
+
+export const joinCompany = async (jwt, userId, companyId, fetchEmployees) => {
+  const uri = import.meta.env.VITE_AUTH_ENDPOINT + `/joinCompany/`;
+  console.log(uri);
+  try {
+    const response = await Axios.patch(
+      uri,
+      { user_id: userId, company_id: companyId },
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+    fetchEmployees();
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const leaveCompany = async (jwt, userId, fetchEmployees) => {
+  const uri = import.meta.env.VITE_AUTH_ENDPOINT + `/leaveCompany/${userId}`;
+  console.log(uri);
+  try {
+    const response = await Axios.patch(
+      uri,
+      { userId },
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+    fetchEmployees();
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const disbandUser = async (
+  buttonId,
+  setButtonLoading,
+  jwt,
+  userId,
+  fetchUsers
+) => {
+  setButtonLoading(buttonId, true);
+  const uri = import.meta.env.VITE_AUTH_ENDPOINT + `/disband/${userId}`;
+  console.log(uri);
+  try {
+    await Axios.patch(
+      uri,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+    setButtonLoading(buttonId, false);
+    notifyDisbandedRequest();
+    fetchUsers();
+  } catch (error) {
+    setButtonLoading(buttonId, false);
+    console.log(error);
+    throw error;
+  }
+};
+
+export const banUser = async (jwt, userId, fetchUsers) => {
+  const uri = import.meta.env.VITE_AUTH_ENDPOINT + `/banUser/${userId}`;
+  try {
+    await Axios.patch(
+      uri,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+    fetchUsers();
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const unbanUser = async (jwt, userId, fetchUsers) => {
+  console.log(userId);
+  const uri = import.meta.env.VITE_AUTH_ENDPOINT + `/unBanUser/${userId}`;
+  try {
+    await Axios.patch(
+      uri,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+    fetchUsers();
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const promoteUser = async (jwt, userId, fetchUsers) => {
+  const uri = import.meta.env.VITE_AUTH_ENDPOINT + `/promoteToAdmin/${userId}`;
+  try {
+    await Axios.patch(
+      uri,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+    fetchUsers();
+  } catch (error) {
     console.log(error);
     throw error;
   }
