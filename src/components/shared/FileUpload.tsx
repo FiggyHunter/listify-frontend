51b2ec7d8e6a9c4cc23523dfc5f68e293a0f7768
FiggyHunter <leonardo.roi3@gmail.com>
2024-02-19 18:27:42 +0100

    users can now upload images to company

--- src/components/shared/FileUpload.tsx
+++ src/components/shared/FileUpload.tsx
@@ -1,4 +1,16 @@
-export default function FileUpload() {
+import imageValidation from "@/utilities/validators/imageValidation";
+import { useState } from "react";
+
+export default function FileUpload({ setCompanyImage }) {
+  const [imageError, setImageError] = useState("");
+  const handleImageChange = (e) => {
+    const selectedFile = e.target.files[0];
+    console.log(e.target);
+    console.log(setCompanyImage);
+    console.log(selectedFile);
+    imageValidation(selectedFile, setCompanyImage, setImageError);
+  };
+
   return (
     <>
       <label
@@ -8,10 +20,18 @@ export default function FileUpload() {
         Upload file
       </label>
       <input
+        onChange={(e) => {
+          handleImageChange(e);
+        }}
         className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
         id="file_input"
         type="file"
       />
+      {imageError !== "" && (
+        <label className="col-span-2 block text-red-500 font-bold">
+          Please upload image in the correct format
+        </label>
+      )}
     </>
   );
 }
