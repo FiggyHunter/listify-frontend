f4afecb57f885403d3c91ea2c8c14b2d281c8743
FiggyHunter <leonardo.roi3@gmail.com>
2024-02-20 17:15:59 +0100

    added company image to company state

--- src/hooks/useCompanyState.ts
+++ src/hooks/useCompanyState.ts
@@ -1,7 +1,8 @@
-import { CreateCompany } from "@/api/company";
+import { CreateCompany, addCompanyImage } from "@/api/company";
 import { useJwtStore } from "@/stores/useUserStore";
 import { useState } from "react";
 import CompanySchema from "@/schemas/companySchema.ts"; // Import your yup schema
+import { imageSchema } from "@/schemas/imageSchema";
 
 const useCompanyState = () => {
   const { jwt } = useJwtStore();
@@ -19,6 +20,10 @@ const useCompanyState = () => {
     hqId: "",
   });
 
+  const [companyImage, setCompanyImage] = useState({});
+
+  console.log(companyImage);
+
   const [errors, setErrors] = useState({});
 
   const handleChange = (event) => {
@@ -32,16 +37,25 @@ const useCompanyState = () => {
   const handleCompanyCreation = async () => {
     try {
       await CompanySchema.validate(companyData, { abortEarly: false });
-      await CreateCompany(companyData, jwt);
+      await imageSchema.validate(
+        { imageFile: companyImage },
+        { abortEarly: false }
+      );
+      const response = await CreateCompany(companyData, jwt);
+      console.log(response);
+      const company_id = response.data.company_id;
+      console.log(company_id);
+      await addCompanyImage(jwt, company_id, companyImage);
       setErrors({});
     } catch (error) {
+      console.log(error);
       if (
         error.response &&
         error?.response?.data?.error === "Company Already Exists"
       )
         setErrors({ companyName: "Company with that name already exists!" });
       const validationErrors = {};
-      error.inner.forEach((err) => {
+      error?.inner?.forEach((err) => {
         validationErrors[err.path] = err.message;
       });
       setErrors(validationErrors);
@@ -54,6 +68,8 @@ const useCompanyState = () => {
     handleChange,
     handleCompanyCreation,
     errors,
+    setCompanyImage,
+    companyImage,
   };
 };
 
