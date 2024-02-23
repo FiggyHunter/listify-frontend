d69a3cfbd5b6fca65ab7f4a654031e7c90145224
FiggyHunter <leonardo.roi3@gmail.com>
2024-02-23 17:17:43 +0100

    promoting to admin

--- src/components/admin/PromoteToAdmin.tsx
+++ src/components/admin/PromoteToAdmin.tsx
@@ -1,7 +1,11 @@
-const PromoteToAdmin = () => {
+import { promoteUser } from "@/api/user";
+
+const PromoteToAdmin = ({ jwt, userId, fetchUsers }) => {
   return (
     <button
-      onClick={() => {}}
+      onClick={() => {
+        promoteUser(jwt, userId, fetchUsers);
+      }}
       className="focus:outline-none  rounded-lg hover:bg-crimsonHover hover:outline-0 border-none transition-all duration-250 bg-crimson font-semibold text-white"
     >
       Promote to Admin
