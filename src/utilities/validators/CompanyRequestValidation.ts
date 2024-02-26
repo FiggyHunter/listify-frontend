1d0e77aea8042051eddad37b7c0b7a31efb0a5ef
FiggyHunter <leonardo.roi3@gmail.com>
2024-02-26 22:40:29 +0100

    added validation logic for company request

---
+++ src/utilities/validators/CompanyRequestValidation.ts
@@ -0,0 +1,19 @@
+import requestSchema from "@/schemas/requestSchema";
+import { ValidationError } from "yup";
+
+export const companyRequestValidation = async (userData, setRequestErrors) => {
+  try {
+    setRequestErrors(null);
+    await requestSchema.validate(userData, {
+      abortEarly: false,
+    });
+  } catch (errors) {
+    console.log(errors);
+    if (errors instanceof ValidationError) {
+      errors.inner.forEach((error) => {
+        setRequestErrors(error.message);
+        throw new Error("Validation Error.");
+      });
+    }
+  }
+};
